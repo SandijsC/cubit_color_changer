4d243316375578b2d2b543a3e71b41843f2dbe96
Sandijs Cirvelis <sandijs-c@inbox.lv>
2021-03-12 23:34:39 +0200

    Add view with buttons that change the background color

--- lib/bloc/cubit/color_cubit.dart
+++ lib/bloc/cubit/color_cubit.dart
@@ -3,7 +3,7 @@ import 'package:bloc/bloc.dart';
 /// Cubit for color state
 class ColorCubit extends Cubit<int> {
   /// Set initial color state
-  ColorCubit() : super(0xFF216600);
+  ColorCubit() : super(0xFF323232);
 
   /// Changing the color
   void setColor(color) => emit(color);
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:cubit_color_changer/bloc/cubit/color_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import './ui/main_view.dart';
 
 void main() {
-  runApp(MaterialApp(home: ColorChanger()));
+  runApp(MaterialApp(
+    home: ColorChanger(),
+    theme: ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: Colors.grey[800],
+      accentColor: Colors.cyan[600],
+      fontFamily: 'Arimo',
+    ),
+  ));
 }
 
 /// App for changing the background color using cubit
@@ -12,7 +21,7 @@ class ColorChanger extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => ColorCubit(),
-      child: const Scaffold(),
+      child: const MainView(),
     );
   }
 }
