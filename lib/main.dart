386f1b8098d581f62e8776d773e3d1630da7bbb2
Sandijs Cirvelis <sandijs-c@inbox.lv>
2021-03-12 14:35:46 +0200

    Create Flutter app.

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(new MaterialApp(home: ColorChanger()));
+}
+
+class ColorChanger extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
